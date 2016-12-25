3760e37be1d07b46004ad6066fd6ab4ee52edcf6
Jake Crane <jake@jakecrane.com>
2016-12-25 15:40:10 -0600

    Add animation for iframe loading

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 $(function() {
     $('#previewContainer').resizable();
+    var $iframe = $('#preview');
+
+    $iframe.load(function ()
+    {
+        $iframe.show();
+        animate($iframe.parent(), 'zoomInDown');
+    });
+
+    function animate($element, animation) {
+        $element.addClass('animated '+ animation);
+        setTimeout( function(){
+            $element.removeClass('animated ' + animation);
+        }, 1500);
+    }
 });
