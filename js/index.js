fac6f6bbd14e07b48fc894224effac3ecac866b3
Jake Crane <jake@jakecrane.com>
2016-12-25 23:28:22 -0600

    More animation and use GSAP instead of animate.css

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
-$(function() {
-    $('#previewContainer').resizable();
-    var $iframe = $('#preview');
+$(function () {
+    const sideNavLis = '.sidenav h3,.sidenav > div > ul > li';
+    const $liveExampleLinks = $('#liveExamples').find('a');
+    const $previewContainer = $('#previewContainer');
+    const $iframe = $('#preview');
 
-    $iframe.load(function ()
-    {
-        $iframe.show();
-        animate($iframe.parent(), 'zoomInDown');
+    $previewContainer.resizable();
+
+    function hideIframe() {
+        TweenMax.to($iframe, .5, {opacity: 0, scale: 0});
+    }
+
+    $liveExampleLinks.click(function () {
+        if ($iframe.is(':visible')) {
+            $(this).attr('target', 'preview');
+        } else {
+            $(this).attr('target', '');
+        }
+        hideIframe();
     });
 
-    function animate($element, animation) {
-        $element.addClass('animated '+ animation);
-        setTimeout( function(){
-            $element.removeClass('animated ' + animation);
-        }, 1500);
-    }
+    $iframe.load(function () {
+        TweenMax.to($iframe, 1, {opacity: 1, scale: 1});
+    });
+    TweenMax.staggerFrom(sideNavLis, .5, {x: -300, rotation: 90, scale: 2}, 0.1);
 });
